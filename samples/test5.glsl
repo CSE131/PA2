468ab87f28fa2ff6327e3bad706c3d6b8f688f4a
Gurkirat Singh <tablagurkirat@yahoo.com>
2017-02-05 02:42:31 -0800

    tests

---
+++ samples/test5.glsl
@@ -0,0 +1,16 @@
+main(int x) {
+    int x = 0;
+    int x[10];
+    (x+x) = vec4(0.55);
+    1 < 2 ? 1 : 2 = x;
+    if (x[10] = i) {
+        break;
+    } else {
+        switch (x = 0) {
+            case 1:
+                y = 10 && k;
+            case 2:
+                x = x + (++foo())-- - test[10];
+        }   
+    }
+    return;
