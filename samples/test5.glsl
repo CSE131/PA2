3ffc25cbdb8adb701d4e79894ad50d162ad2a042
Gurkirat Singh <cs131wbq@ieng6-203.ucsd.edu>
2017-02-05 13:32:59 -0800

    more

--- samples/test5.glsl
+++ samples/test5.glsl
@@ -1,10 +1,14 @@
 int main(int x) {
-    int x = 0;
+    int x = -1; 
     int x[10];
     (x+x) = vec4(0.55);
     1 < 2 ? 1 : 2 = x;
     if (x[10] = i) {
         break;
+        for(;;i) {}
+        do {
+            int i = -3 + +4 * -+-+-+fff;
+        } while (g || y <= asdf);
     } else {
         switch (x = 0) {
             case 1:
@@ -12,6 +16,6 @@ int main(int x) {
             case 2:
                 x = x + (++foo())-- - test[10];
         }   
-    }
-    return true && false;
+    }   
+    return;
 }
---
+++ samples/test6.glsl
@@ -0,0 +1,11 @@
+void foo(int x){ 
+    int i = -1;
+    int z = i++;
+    int a = +123;
+    int x = +-+-+-2;
+    ++i;
+    x = -1;
+    x = x + 3;
+    y = 2 + 2;
+
+}
